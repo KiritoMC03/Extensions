fe939a0d63a1240d023d78e7d21b76cdace2dc8b
Kirill Dudnik <kirill.dudnik@balaganov.rocks>
2023-02-14 12:40:26 +0100

    - feat : more extensions!

---
+++ Source/EmptyMonoBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace JoyKirito.Extensions
+{
+    public class EmptyMonoBehaviour : MonoBehaviour
+    {
+    }
+}
---
+++ Source/NavMeshAgentExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using JoyKirito.Extensions;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace JoyKirito.Extensions
+{
+    public static class NavMeshAgentExtensions
+    {
+        public static void SetDestination(this NavMeshAgent agent, Vector3 target, Action onComplete)
+        {
+            agent.SetDestination(target);
+            agent.InvokeOnComplete(onComplete);
+        }
+        
+        public static void InvokeOnComplete(this NavMeshAgent agent, Action action)
+        {
+            if (agent.IsNull() || action.IsNull()) return;
+            if (!agent.TryGetComponent(out EmptyMonoBehaviour emptyMonoBehaviour))
+                emptyMonoBehaviour = agent.gameObject.AddComponent<EmptyMonoBehaviour>();
+            emptyMonoBehaviour.StartCoroutine(WaitCompletionRoutine(agent, action, emptyMonoBehaviour));
+        }
+
+        private static IEnumerator WaitCompletionRoutine(NavMeshAgent agent, Action action,
+            EmptyMonoBehaviour emptyMonoBehaviour)
+        {
+            Transform transform = agent.transform;
+            Vector3 targetPose = agent.destination;
+            while (agent.velocity.sqrMagnitude > 0.01f || 
+                   Vector3.Distance(transform.position, targetPose) > 0.1f)
+                yield return null;
+            UnityEngine.Object.DestroyImmediate(emptyMonoBehaviour);
+            action?.Invoke();
+        }
+    }
+}
